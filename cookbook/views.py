adcfd0efe3536cfb03a384ef4818eaebe74ffb59
rafaelmarquespn <rafaelmarquespn>
2023-03-09 13:37:06 -0300

    banco de dados com as receitas

--- cookbook/views.py
+++ cookbook/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from .models import Receita
+
 
 def index(request):
-    receitas = {
-        1: 'Lasanha',
-        2: 'Sopa de Legumes',
-        3: 'Sorvete'
+    receitas = Receita.objects.all()
+    dados = {
+        'receitas' : receitas
     }
-    return render(request, 'index.html', 
-                  {'nome_da_receita': receitas}
-                  )
 
-def receita(request):
-    return render(request, 'receita.html')
+    return render(request, 'index.html', dados)
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+    receita_a_exibir = {
+        'receita' : receita
+    }
+    return render(request, 'receita.html', receita_a_exibir)
